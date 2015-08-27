bbf5566124f75765de0ce2cbd2940944bcbc7c7a
kennypybot <kennypybot@gmail.com>
2015-08-27 23:56:56 +0200

    Small update

---
+++ Sunlapse v2.py
@@ -0,0 +1,6 @@
+"""
+Scrape a website for sundown and sunset values.
+If site is unavailable, use default values.
+Start timelapse at the right values.
+
+"""
--- speeltuin.py
+++ speeltuin.py
@@ -1,3 +1,7 @@
-a = 'abcdefghij'
-a -= a - 'bcdef'
-print(a)
+#This is just a file I use as playground to test stuff.
+
+with open('mail_creds.txt', 'r') as f:
+    text_data = f.read()
+sender = text_data.split('+')[0]
+sender_pass = text_data.split('+')[1]
+print(sender, sender_pass)
